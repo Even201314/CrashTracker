d6a7215a1825ed484113abddafd62108fb337ac5
1157689755@qq.com <184175>
2016-01-07 20:27:53 +0800

    修改UI

--- app/src/main/java/com/even/crashtracker/CrashTracker.java
+++ app/src/main/java/com/even/crashtracker/CrashTracker.java
@@ -7,7 +7,152 @@ import android.content.Context;
  * Created by Even on 2016/1/6.
  */
 public class CrashTracker {
+    private static boolean cause = true;
+    private static boolean stackTrace = true;
+    private static boolean sdk = true;
+    private static boolean appVersion = true;
+    private static boolean className = false;
+    private static boolean message = false;
+    private static boolean localizedMessage = false;
+    private static boolean brand = false;
+    private static boolean device = false;
+    private static boolean model = false;
+    private static boolean product = false;
+    private static boolean release = false;
+    private static boolean incremental = false;
+    private static boolean height = false;
+    private static boolean width = false;
+    private static boolean tablet = false;
+
     public static void init(Context context) {
         Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(context));
+    }
+
+    public static boolean isCause() {
+        return cause;
+    }
+
+    public static void setCause(boolean cause) {
+        CrashTracker.cause = cause;
+    }
+
+    public static boolean isStackTrace() {
+        return stackTrace;
+    }
+
+    public static void setStackTrace(boolean stackTrace) {
+        CrashTracker.stackTrace = stackTrace;
+    }
+
+    public static boolean isSdk() {
+        return sdk;
+    }
+
+    public static void setSdk(boolean sdk) {
+        CrashTracker.sdk = sdk;
+    }
+
+    public static boolean isAppVersion() {
+        return appVersion;
+    }
+
+    public static void setAppVersion(boolean appVersion) {
+        CrashTracker.appVersion = appVersion;
+    }
+
+    public static boolean isClassName() {
+        return className;
+    }
+
+    public static void setClassName(boolean className) {
+        CrashTracker.className = className;
+    }
+
+    public static boolean isMessage() {
+        return message;
+    }
+
+    public static void setMessage(boolean message) {
+        CrashTracker.message = message;
+    }
+
+    public static boolean isLocalizedMessage() {
+        return localizedMessage;
+    }
+
+    public static void setLocalizedMessage(boolean localizedMessage) {
+        CrashTracker.localizedMessage = localizedMessage;
+    }
+
+    public static boolean isBrand() {
+        return brand;
+    }
+
+    public static void setBrand(boolean brand) {
+        CrashTracker.brand = brand;
+    }
+
+    public static boolean isDevice() {
+        return device;
+    }
+
+    public static void setDevice(boolean device) {
+        CrashTracker.device = device;
+    }
+
+    public static boolean isModel() {
+        return model;
+    }
+
+    public static void setModel(boolean model) {
+        CrashTracker.model = model;
+    }
+
+    public static boolean isProduct() {
+        return product;
+    }
+
+    public static void setProduct(boolean product) {
+        CrashTracker.product = product;
+    }
+
+    public static boolean isRelease() {
+        return release;
+    }
+
+    public static void setRelease(boolean release) {
+        CrashTracker.release = release;
+    }
+
+    public static boolean isIncremental() {
+        return incremental;
+    }
+
+    public static void setIncremental(boolean incremental) {
+        CrashTracker.incremental = incremental;
+    }
+
+    public static boolean isHeight() {
+        return height;
+    }
+
+    public static void setHeight(boolean height) {
+        CrashTracker.height = height;
+    }
+
+    public static boolean isWidth() {
+        return width;
+    }
+
+    public static void setWidth(boolean width) {
+        CrashTracker.width = width;
+    }
+
+    public static boolean isTablet() {
+        return tablet;
+    }
+
+    public static void setTablet(boolean tablet) {
+        CrashTracker.tablet = tablet;
     }
 }
--- app/src/main/java/com/even/crashtracker/activity/CrashActivity.java
+++ app/src/main/java/com/even/crashtracker/activity/CrashActivity.java
@@ -1,10 +1,15 @@
 package com.even.crashtracker.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
 import android.widget.TextView;
 
+import com.even.crashtracker.CrashTracker;
 import com.even.crashtracker.R;
 import com.even.crashtracker.model.Tracker;
 import com.google.gson.Gson;
@@ -23,6 +28,89 @@ public class CrashActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Tracker tracker = gson.fromJson(trackerInfo, Tracker.class);
         TextView tvCrashInfo = (TextView) findViewById(R.id.tv_crash_info);
-        tvCrashInfo.setText(trackerInfo);
+
+        StringBuilder sb = new StringBuilder();
+        if (CrashTracker.isSdk()) {
+            sb.append(getString(R.string.crash_tracker_sdk)).append(" : ").append
+                    (getSpannableString(tracker.getSDK
+                            ())).append("\n");
+        }
+        if (CrashTracker.isAppVersion()) {
+            sb.append(getString(R.string.crash_tracker_app_version)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getApp_Version())).append("\n");
+        }
+        if (CrashTracker.isClassName()) {
+            sb.append(getString(R.string.crash_tracker_class_name)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getClassName())).append("\n");
+        }
+        if (CrashTracker.isCause()) {
+            sb.append(getString(R.string.crash_tracker_cause)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getCause())).append("\n");
+        }
+        if (CrashTracker.isStackTrace()) {
+            sb.append(getString(R.string.crash_tracker_stack_trace)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getStack_Trace())).append("\n");
+        }
+        if (CrashTracker.isMessage()) {
+            sb.append(getString(R.string.crash_tracker_message)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getMessage())).append("\n");
+        }
+        if (CrashTracker.isLocalizedMessage()) {
+            sb.append(getString(R.string.crash_tracker_localized_message)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getLocalized_Message())).append("\n");
+        }
+        if (CrashTracker.isBrand()) {
+            sb.append(getString(R.string.crash_tracker_brand)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getBrand())).append("\n");
+        }
+        if (CrashTracker.isDevice()) {
+            sb.append(getString(R.string.crash_tracker_device)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getDevice())).append("\n");
+        }
+        if (CrashTracker.isModel()) {
+            sb.append(getString(R.string.crash_tracker_model)).append(" : ").append(tracker
+                    .getModel()).append("\n");
+        }
+        if (CrashTracker.isProduct()) {
+            sb.append(getString(R.string.crash_tracker_product)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getProduct())).append("\n");
+        }
+        if (CrashTracker.isIncremental()) {
+            sb.append(getString(R.string.crash_tracker_incremental)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getIncremental())).append("\n");
+        }
+        if (CrashTracker.isHeight()) {
+            sb.append(getString(R.string.crash_tracker_height)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getHeight())).append("\n");
+        }
+        if (CrashTracker.isWidth()) {
+            sb.append(getString(R.string.crash_tracker_width)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getWidth())).append("\n");
+        }
+        if (CrashTracker.isTablet()) {
+            sb.append(getString(R.string.crash_tracker_tablet)).append(" : ").append
+                    (getSpannableString(tracker
+                            .getTablet())).append("\n");
+        }
+        tvCrashInfo.setText(sb);
+    }
+
+    private SpannableString getSpannableString(String content) {
+        SpannableString spannableString = new SpannableString(content);
+        spannableString.setSpan(new BackgroundColorSpan(Color.GRAY), 0, spannableString.length(),
+                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        return spannableString;
     }
 }
